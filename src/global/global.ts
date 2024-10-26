8e98762e720406e8e95094b8ecdb231872ac9bae
Awinash Kumar Jaiswal <awinashjaiswal100@gmail.com>
2024-10-26 19:26:00 +0530

    improved collapsible

--- src/global/global.ts
+++ src/global/global.ts
@@ -4,7 +4,7 @@ import * as OCinput from '../components/oc-input/meta.json'
 import * as OCcontainer from '../components/oc-container/meta.json';
 import * as OcCard from "../components/oc-card/meta.json";
 import * as OcCarousel  from "../components/oc-carousel/meta.json";
-import * as OcCollapsible  from "../components/oc-collapsible/oc-collapsible";
+import * as OcCollapsible  from "../components/oc-collapsible/meta.json";
 import * as OcColumn from "../components/oc-column/meta.json";
 import * as OcHeader  from "../components/oc-header/meta.json";
 import * as OcHtmlEditor from "../components/oc-html-editor/meta.json";
